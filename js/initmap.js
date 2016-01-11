cc21e80a6d1c04894a88869dfb628dcc17f60357
Petr <petr@edgebox.net>
2016-01-11 08:17:52 +0000

    Enable click to select coordinates

--- js/initmap.js
+++ js/initmap.js
@@ -5,4 +5,26 @@ function initMap(){
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    marker = new google.maps.Marker({map: map, draggable: true});
+    
+    google.maps.event.addListener(map, 'click', function(e){
+        marker.setPosition(e.latLng);
+        marker.setMap(map);
+        updateCoords(e.latLng);
+    });
+    
+    google.maps.event.addListener(marker, 'dragend', function(){
+        updateCoords(marker.getPosition());    
+    });
+}
+
+function updateCoords(latLng){
+    $('#gpsLatInput').val(latLng.lat());
+    $('#gpsLonInput').val(latLng.lng());
+}
+
+function clearGPS(){
+    marker.setMap(null);
+    $('#gpsLatInput').val("");
+    $('#gpsLonInput').val("");
 }
